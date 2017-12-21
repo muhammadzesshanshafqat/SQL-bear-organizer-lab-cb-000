0003cb1fa38ed3c222f7b07720673cb736a8fb3f
Muhammad Zeeshan Shafqat <101041699@student.swin.edu.au>
2017-12-21 01:55:11 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+Values ("Mr.Chocolate", 20, "M", "dark brown", "Friendly", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+Values ("Rowdy", 10, "M", "Black", "Friendly", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+Values ("Tabitha", 6, "F", "dark brown", "Super-Friendly", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+Values ("Sergeant Brown", 5, "M", " dark brown", "goofy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+Values ("Melissa", 13, "F", "White", "Friendly", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+Values ("Grinch", 2, "M", "Brown", "Angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+Values ("Wendy", 6, "F", "Brown", "Sexual", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+Values (NULL, 20, "M", "Black", "aggressive", 0);
